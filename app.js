c6597cf6f0ea479fe4ef69746301a2a68daadebe
Guga <g.juma.tm@gmail.com>
2021-09-30 08:21:57 +0800

    port changed

--- app.js
+++ app.js
@@ -100,7 +100,7 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err });
 })
 //==========================================================================
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`App is listening at port ${port}`)
 })
